a45f7222f870231d1d9cc278f8e22f3c1408684b
Jonas Alves <joonasbalves@gmail.com>
2022-04-09 15:10:59 -0300

    Maranhao Legal

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+
+import '../assets/scss/main.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+    
+        <title>Maranhã Legal</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
